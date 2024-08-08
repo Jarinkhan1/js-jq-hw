ae4bf2886f4d4644b160e9b24e8d6c42c126d87e
Jarin khan <168277348+Jarinkhan1@users.noreply.github.com>
2024-08-08 01:11:00 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+/*ex one*/
+
+function showText(){
+   document.getElementById('s_text').innerHTML ="step to learn web development";
+}
+
+
+// ex two
+
+function showTextTwo(){
+    document.getElementById('s_text_two').innerHTML ="Hello bangladesh";
+}
+
+// ex three
+
+function showTextThree(){
+    document.getElementById('s_text_three').innerHTML ="Step to learn web development";
+}
+
+// ex four
+
+function showTextFour(){
+    document.getElementById('s_text_four').innerHTML ="Developing front end website";
+}
+
+// ex five
+
+function showTextFive(){
+    document.getElementById('s_text_five').innerHTML ="Understanding user interface and user experience";
+}
+
+// ex six
+
+function showTextSix(){
+    document.getElementById('s_text_six').innerHTML ="Writing technical documentation";
+}
+
+// ex seven
+
+function showTextSeven(){
+    document.getElementById('s_text_seven').innerHTML ="Designing website architecture";
+}
+
+// ex eight
+
+function showTextEight(){
+    document.getElementById('s_text_eight').innerHTML ="Implementing data secuirity protocol";
+}
+
+// ex nine
+
+function showTextNine(){
+    document.getElementById('s_text_nine').innerHTML ="Creating service and databases";
+}
+
+// example ten
+
+function showTextTen(){
+    document.getElementById('s_text_ten').innerHTML ="Keep me in yours prayers";
+}
